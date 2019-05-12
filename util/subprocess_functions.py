395a1f53186db9d8f985caf045433405ef123081
Matt-Gleich <matthewgleich@gmail.com>
2019-05-12 14:13:15 -0400

    Create get_subprocess_output function

--- util/subprocess_functions.py
+++ util/subprocess_functions.py
@@ -7,11 +7,15 @@ def get_subprocess_output(shell_command):
     :param shell_command: the command that was recorded.
     :return: the command's output
     """
-
-    return complete_output
+    string_command = str(shell_command)
+    stdout_position = string_command.find("stdout")
+    stderr_position = string_command.find("stderr")
+    relative_string = string_command[stdout_position:stderr_position]
+    final_string = relative_string[relative_string.find("'") + 1:-3]
+    return final_string
 
 
 # Testing:
-command = str((subprocess.run(['ls'], capture_output=True)))
-print(command)
+command = str((subprocess.run(['pwd'], capture_output=True)))
+# print(command)
 print(get_subprocess_output(command))
