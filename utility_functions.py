24c42c74d89f7e66af5d3d66e32ef4540f7f2f37
Matthew Gleich <matthewgleich@gmail.com>
2019-05-15 23:28:45 -0400

    Create utility_functions.py

---
+++ utility_functions.py
@@ -0,0 +1,38 @@
+import subprocess
+
+
+def get_subprocess_output(subprocess_command):
+    """
+    Will get the output of a subprocess command that has been ran.
+    :param subprocess_command: the command that was recorded.
+    :return: the command's output
+    """
+    string_command = str(subprocess_command)
+    stdout_position = string_command.find("stdout")
+    stderr_position = string_command.find("stderr")
+    relative_string = string_command[stdout_position:stderr_position]
+    final_string = relative_string[relative_string.find("'") + 1:-3]
+    return final_string
+
+
+# Testing:
+# command = str((subprocess.run(['pwd'], capture_output=True)))
+# print(command)
+# print(get_subprocess_output(command))
+
+
+def run_command(shell_command, get_output):
+    """
+    Will run a shell command using the subprocess module
+    :param shell_command: The command that is going to be ran
+    :param get_output: Will capture the output of the command
+    :return: the command output
+    """
+    command_ran = subprocess.run(shell_command, capture_output=get_output)
+    return command_ran
+
+
+# Testing:
+# print(run_command("find . -type f", True))
+# Testing with get_subprocess_output:
+# print(get_subprocess_output(run_command("pwd", True)))
