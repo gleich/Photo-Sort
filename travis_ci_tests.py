82a3a564aba0607d1248d9bd77cb582da0831776
Matthew Gleich <matthewgleich@gmail.com>
2019-05-18 17:03:51 -0400

    Remove get_photo_creation_date function

--- travis_ci_tests.py
+++ travis_ci_tests.py
@@ -41,20 +41,6 @@ class TestExifData(unittest.TestCase):
         self.assertEqual(result["c"], "d")
 
 
-    def test_get_file_creation_date(self):
-        """
-        Tests the get_file_creation_date funtion
-        """
-        result = utility_functions.file_creation_date('./photos/test_image.jpg')
-        self.assertEqual(str(type(result)), "<class 'list'>")
-        self.assertEqual(str(type(result[0])), "<class 'int'>")
-        self.assertEqual(str(type(result[1])), "<class 'int'>")
-        self.assertEqual(str(type(result[2])), "<class 'int'>")
-        self.assertEqual(result[0], 5)
-        self.assertEqual(result[1], 13)
-        self.assertEqual(result[2], 2019)
-
-
     ###############################################
     #Testing the functions in photo_functions.py#
     ###############################################
