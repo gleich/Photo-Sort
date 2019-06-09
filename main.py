f4cb49ecea5b0cad665336af723c3efc25238f18
Matthew Gleich <matthewgleich@gmail.com>
2019-06-09 00:02:29 -0400

    Add blank line

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ def main():
         print("Created the folder:", folder)
     UF.print_colored("Created " + str(len(created_folders)) + " folders", "green")
     UF.clear_output(10)
-    question_parts = ["Are you sure that you wanna move all the photos into the folders?", colored("WARNING:", "red", attrs=['bold', 'blink']), "1. If you quit the program now, you will need to run the program again by running python main.py in terminal.", "2. The folders were already created, so you will need to run the command to just put the photos in folders.", "Answer with y or n"]
+    question_parts = ["Are you sure that you wanna move all the photos into the folders?", colored("WARNING:", "red", attrs=['bold', 'blink']), "1. If you quit the program now, you will need to run the program again by running python main.py in terminal.", "2. The folders were already created, so you will need to run the command to just put the photos in folders.", "Answer with y or n\n"]
     UF.clear_output(50)
     UF.print_colored("Created " + str(len(created_folders)) + " folders", "green")
     UF.print_colored("Found exif data for " + str(len(exif_data)) + " photos", "green")
