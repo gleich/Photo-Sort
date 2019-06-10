bb6fed95a86712869c22a031813c9fc4ef6ddcf8
Matthew Gleich <matthewgleich@gmail.com>
2019-06-10 16:18:03 -0400

    Update update_settings.py

--- setup.py
+++ setup.py
@@ -11,35 +11,11 @@ def update_settings():
     if "windows" in current_os.lower() or "linux" in current_os.lower():
         raise Exception("This program is not supported for your Operating System.")
     with open("settings.txt", "w") as settings_file:
-        settings_file.write("Operating system: " + current_os)
-        settings_file.write("Ran before: " + str(True))
-        settings_file.write("Last Run Time: " + str(current_time))
-
-
-def setup_requirements():
-    """
-    Will install and setup decencies
-    :return:
-    """
-    current_os = platform()
-    if "windows" in current_os.lower() or "linux" in current_os.lower():
-        raise Exception("This program doesn't work on Linux or Windows")
-    current_time = datetime.datetime.now()
-    with open("settings.json", "r") as settings_json:
-        loaded_list = json.load(settings_json)
-    list_copy = [
-        "Operating System:",
-         current_os,
-        "Ran Before:",
-        True,
-        "Times Ran:",
-        loaded_list[6] + 1,
-        "Last Run:",
-        current_time
-    ]
-    with open("settings.json", "w") as settings_json:
-        json.dump(list_copy, settings_json)
+        settings_file.write("Operating system: " + current_os + "\n")
+        settings_file.write("Ran before: " + str(True) + "\n")
+        settings_file.write("Last Run Time: " + str(current_time) + "\n")
 
 
 
-
+# Testing:
+# update_settings()
