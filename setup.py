77d558e8e709eba5357fdcd21f09114c7c28ce93
Matthew Gleich <matthewgleich@gmail.com>
2019-06-09 21:17:31 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,16 +9,20 @@ def init_settings():
     """
     current_os = platform()
     if "windows" in current_os.lower() or "linux" in current_os.lower():
-        raise "Can't "
-
+        raise Exception("This program doesn't work on Linux or Windows")
     current_time = datetime.datetime.now()
-    dict_to_load = {"Operating System": current_os,
-                    "Ran Before:": False,
-                    "Run Times:": 0,
-                    "Last run:": current_time
-                    }
+    list_to_load = [
+        "Operating System:",
+        current_os,
+        "Ran Before:",
+        False,
+        "Times Ran:",
+        0,
+        "Last Run:",
+        current_time
+    ]
     with open("settings.json", "w") as settings_json:
-        json.dump(dict_to_load, settings_json)
+        json.dump(list_to_load, settings_json)
 
 
 def update_settings():
@@ -26,4 +30,22 @@ def update_settings():
     Will update the user's settings
     :return: none
     """
+    current_os = platform()
+    if "windows" in current_os.lower() or "linux" in current_os.lower():
+        raise Exception("This program doesn't work on Linux or Windows")
+    current_time = datetime.datetime.now()
+    with open("settings.json", "r") as settings_json:
+        loaded_list = json.load(settings_json)
+    list_copy = [
+        "Operating System:",
+         current_os,
+        "Ran Before:",
+        True,
+        "Times Ran:",
+        loaded_list[6] + 1,
+        "Last Run:",
+        current_time
+    ]
+    with open("settings.json", "w") as settings_json:
+        json.dump(list_copy, settings_json)
 
