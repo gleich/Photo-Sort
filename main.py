3ca14e66e3d5cc4e455f9dcf1424fddcd7da8d13
Matthew Gleich <matthewgleich@gmail.com>
2019-06-15 22:45:26 -0400

    Add command usage

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import file_management_functions as FMF
 import utility_functions as UF
 from termcolor import colored
 from platform import platform
+import json
 
 
 def main():
@@ -17,59 +18,73 @@ def main():
         current_platform = platform()
         if "windows" in current_platform.lower() or "linux" in current_platform.lower():
             raise Exception("This program doesn't support Linux or Windows. It will not be able to run.")
-        file_types = FMF.pre_import_file_types()
-        print()
         UF.clear_output(50)
-        question_items = ["Where is the folder you wanna sort?", "1. External Drive", "2. Folder", "3. Folder on External Drive\n---------------------------------\n"]
-        UF.clear_output(50)
-        question = "\n".join(question_items)
-        while True:
-            drive_or_folder = input(question)
-            if "folder" in drive_or_folder.lower() and "drive" in drive_or_folder.lower():
-                FMF.cd_into_drive()
-                FMF.cd_into_folder(False)
-                break
-            elif "folder" in drive_or_folder.lower():
-                FMF.cd_into_folder(True)
-                break
-            elif "drive" in drive_or_folder.lower():
-                FMF.cd_into_drive()
-                break
-            else:
-                UF.print_colored("Please pick a valid option!", "red")
-                continue
-        image_paths = PF.list_image_paths(file_types)
-        UF.clear_output(50)
-        for image in image_paths:
-            print("Found:", image)
-        UF.print_colored("Found " + str(len(image_paths)) + " image paths", 'green')
-        UF.clear_output(10)
-        exif_data = PF.photo_exif_data(image_paths)
-        for file in exif_data:
-            file_name = file["File Name"]
-            print("Got the exif data for:", file_name)
-        UF.print_colored("Found exif data for " + str(len(exif_data)) + " photos", "green")
-        UF.clear_output(10)
-        created_folders = FMF.init_folders(exif_data)
-        for folder in created_folders:
-            print("Created the folder:", folder)
-        UF.print_colored("Created " + str(len(created_folders)) + " folders", "green")
-        UF.clear_output(10)
-        question_parts = ["Are you sure that you wanna move all the photos into the folders?", colored("WARNING:", "red", attrs=['bold', 'blink']), "1. If you quit the program now, you will need to run the program again by running python main.py in terminal.", "2. The folders were already created, so you will need to run the command to just put the photos in folders.", colored("Answer with y or n\n", "yellow", attrs=["bold"])]
-        UF.clear_output(50)
-        print("---------------------------------")
-        UF.print_colored("Created " + str(len(created_folders)) + " folders", "green")
-        UF.print_colored("Found exif data for " + str(len(exif_data)) + " photos", "green")
-        UF.print_colored("Found " + str(len(image_paths)) + " image paths", 'green')
+        commands = ["sort folder and it's sub-folders", "sort current folder only", "move files from duplicates folder"]
+        print("Below is a list of all the commands. Please select one.\n")
+        command_number = 0
+        for command in commands:
+            command_number += 1
+            print("{one}. {two}".format(one=command_number, two=command))
         print("")
-        continue_question = input("\n".join(question_parts))
-        if "y" in continue_question.lower():
-            duplicate_amount = FMF.put_photos_in_folders(exif_data)
-            UF.print_colored("Found " + str(duplicate_amount) + " Duplicates", "green")
-            UF.print_colored("All the photos were moved into their folders", 'green')
-        elif "n" in continue_question.lower():
+        command_to_use = input("Which of the commands shown above would you like to use?\n").lower()
+        if "sort" in command_to_use and "folder" in command_to_use:
+            file_types = FMF.pre_import_file_types()
+            print()
+            UF.clear_output(50)
+            question_items = ["Where is the folder you wanna sort?", "1. External Drive", "2. Folder", "3. Folder on External Drive\n---------------------------------\n"]
+            UF.clear_output(50)
+            question = "\n".join(question_items)
+            while True:
+                drive_or_folder = input(question)
+                if "folder" in drive_or_folder.lower() and "drive" in drive_or_folder.lower():
+                    FMF.cd_into_drive()
+                    FMF.cd_into_folder(False)
+                    break
+                elif "folder" in drive_or_folder.lower():
+                    FMF.cd_into_folder(True)
+                    break
+                elif "drive" in drive_or_folder.lower():
+                    FMF.cd_into_drive()
+                    break
+                else:
+                    UF.print_colored("Please pick a valid option!", "red")
+                    continue
+            if "sort folder" in command_to_use and "sub-folders" in command_to_use or "1" in command_to_use:
+                image_paths = PF.list_image_paths(file_types, True)
+            else:
+                image_paths = PF.list_image_paths(file_types, False)
+                print(image_paths)
+            UF.clear_output(50)
+            for image in image_paths:
+                print("Found:", image)
+            UF.print_colored("Found " + str(len(image_paths)) + " image paths", 'green')
             UF.clear_output(10)
-            print(colored("The photos were not put in the folders.", 'yellow', attrs=['bold', 'blink']))
+            exif_data = PF.photo_exif_data(image_paths)
+            for file in exif_data:
+                file_name = file["File Name"]
+                print("Got the exif data for:", file_name)
+            UF.print_colored("Found exif data for " + str(len(exif_data)) + " photos", "green")
+            UF.clear_output(10)
+            created_folders = FMF.init_folders(exif_data)
+            for folder in created_folders:
+                print("Created the folder:", folder)
+            UF.print_colored("Created " + str(len(created_folders)) + " folders", "green")
+            UF.clear_output(10)
+            question_parts = ["Are you sure that you wanna move all the photos into the folders?", colored("WARNING:", "red", attrs=['bold', 'blink']), "1. If you quit the program now, you will need to run the program again by running python main.py in terminal.", "2. The folders were already created, so you will need to run the command to just put the photos in folders.", colored("Answer with y or n\n", "yellow", attrs=["bold"])]
+            UF.clear_output(50)
+            print("---------------------------------")
+            UF.print_colored("Created " + str(len(created_folders)) + " folders", "green")
+            UF.print_colored("Found exif data for " + str(len(exif_data)) + " photos", "green")
+            UF.print_colored("Found " + str(len(image_paths)) + " image paths", 'green')
+            print("")
+            continue_question = input("\n".join(question_parts))
+            if "y" in continue_question.lower():
+                duplicate_amount = FMF.put_photos_in_folders(exif_data)
+                UF.print_colored("Found " + str(duplicate_amount) + " Duplicates", "green")
+                UF.print_colored("All the photos were moved into their folders", 'green')
+            elif "n" in continue_question.lower():
+                UF.clear_output(10)
+                print(colored("The photos were not put in the folders.", 'yellow', attrs=['bold', 'blink']))
     else:
         print("The program will not run. To restart it run python main.py")
 
