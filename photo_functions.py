241015180ae99832210956a5c01983f8e1201872
Matthew Gleich <matthewgleich@gmail.com>
2019-06-15 22:45:09 -0400

    Add current folder to list_image_paths

--- photo_functions.py
+++ photo_functions.py
@@ -1,27 +1,35 @@
 import utility_functions as UF
 import file_management_functions as FMF
+import os
 
 
-def list_image_paths(file_types):
+def list_image_paths(file_types, sub_folders):
     """
     Will list all the photos in the current directory and in all subdirectories.
     :param file_types: array of the supported file types
+    :param sub_folders: if it should just be the current folder or current folder and sub folders.
     :return: array of the photo paths
     """
     files = []
-    for extension in file_types:
-        command_to_run = ['find', '.', '-iname', '*' + extension + '']
-        ran_command = UF.run_command(command_to_run, True)
-        command_output = UF.get_subprocess_output(ran_command)
-        if "\\n" in command_output:
-            file_paths = command_output.split("\\n")
-            for file in file_paths:
-                if file != '':
-                    strip_1 = file.strip("\\n")
-                    files.append(strip_1)
-        else:
-            if command_output != '':
-                files.append(command_output)
+    if sub_folders:
+        for extension in file_types:
+            command_to_run = ['find', '.', '-iname', '*' + extension + '']
+            ran_command = UF.run_command(command_to_run, True)
+            command_output = UF.get_subprocess_output(ran_command)
+            if "\\n" in command_output:
+                file_paths = command_output.split("\\n")
+                for file in file_paths:
+                    if file != '':
+                        strip_1 = file.strip("\\n")
+                        files.append(strip_1)
+            else:
+                if command_output != '':
+                    files.append(command_output)
+    else:
+        for extension in file_types:
+            for file in os.listdir("."):
+                if file.endswith(extension):
+                    files.append(file)
     return files
 
 
